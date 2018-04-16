0eafc275b8bab4b5ce335fb20a7f7eb04efe149a
doorlock33 <34480077+doorlock33@users.noreply.github.com>
2018-04-16 17:26:57 +0200

    Initial commit

---
+++ CeeLearnAndDo/Controllers/PortfolioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CeeLearnAndDo.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public ActionResult OneColumn()
+        {
+            return View();
+        }
+
+        public ActionResult TwoColumn()
+        {
+            return View();
+        }
+
+        public ActionResult ThreeColumn()
+        {
+            return View();
+        }
+
+        public ActionResult SingleItem()
+        {
+            return View();
+        }
+    }
+}
